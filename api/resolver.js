cf13a1d05034a166097a7e4739147623bb93cdba
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-05-05 12:51:41 +0330

    register section 1-2

--- api/resolver.js
+++ api/resolver.js
@@ -1,8 +1,23 @@
+const User = require('app/models/users');
 const resolvers = {
     Query: {
         user : () => {
             return "ali"
         }
-    } 
+    } ,
+    Mutation: {
+        register : async (param, args) => {
+               await User.create({
+                    phone: args.phone,
+                    password : args.password,
+                })
+                return{
+                    status: 200,
+                    message: 'اطلاعات شما در سیستم ذخیره شد.'
+                }
+        }
+    }
+    
+
 }
 module.exports = resolvers
